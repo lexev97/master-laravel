203aaa61aae56364112663fd565aeb9299fb2ec3
Lexev <evtenkoaleksei@gmail.com>
2024-03-20 13:53:07 +0300

    24. Layouts Using Template Inheritance

--- l10-task-list/resources/views/index.blade.php
+++ l10-task-list/resources/views/index.blade.php
@@ -3,14 +3,11 @@
 @section('title', 'The list of tasks')
 
 @section('content')
-	{{-- @if (count($tasks)) --}}
-	@forelse ($tasks as $task)
-		<div>
-			<a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
-		</div>
-	@empty
-	<div>There are no tasks!</div>
-	@endforelse
-
-	{{-- @endif --}}
+@forelse ($tasks as $task)
+<div>
+	<a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+</div>
+@empty
+<div>There are no tasks!</div>
+@endforelse
 @endsection
--- l10-task-list/resources/views/layouts/app.blade.php
+++ l10-task-list/resources/views/layouts/app.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Laravel Task List App</title>
-	</head>
-	<body>
-		<h1>@yield('title')</h1>
-		<div>
-			@yield('content')
-		</div>
-	</body>
+
+<head>
+	<title>Laravel Task List App</title>
+</head>
+
+<body>
+	<h1>@yield('title')</h1>
+	<div>
+		@yield('content')
+	</div>
+</body>
+
 </html>
