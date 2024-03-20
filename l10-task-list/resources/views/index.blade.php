9db7395179ddcb4c015eeef2bce9ddd5a2799c7a
Lexev <evtenkoaleksei@gmail.com>
2024-03-20 13:46:39 +0300

    [200~воспроизведено

--- l10-task-list/resources/views/index.blade.php
+++ l10-task-list/resources/views/index.blade.php
@@ -1,6 +1,8 @@
-<h1>The list of tasks</h1>
+@extends('layouts.app')
 
-<div>
+@section('title', 'The list of tasks')
+
+@section('content')
 	{{-- @if (count($tasks)) --}}
 	@forelse ($tasks as $task)
 		<div>
@@ -11,4 +13,4 @@
 	@endforelse
 
 	{{-- @endif --}}
-</div>
+@endsection
