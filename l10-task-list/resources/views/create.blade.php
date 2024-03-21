3e9e3d8c9e90a5c17be00a68b9fb4de70d4fe4fd
Lexev <evtenkoaleksei@gmail.com>
2024-03-21 17:17:19 +0300

    35. Reusability (Route Model Binding Form Requests, Mass Assignment

--- l10-task-list/resources/views/create.blade.php
+++ l10-task-list/resources/views/create.blade.php
@@ -15,21 +15,21 @@
 	@csrf
 	<div>
 		<label for="title">Title</label>
-		<input type="text" id="title" name="title">
+		<input type="text" id="title" name="title" value="{{ old('title') }}">
 		@error('title')
 			<span class="error-message">{{ $message }}</span>
 		@enderror
 	</div>
 	<div>
 		<label for="description">Description</label>
-		<textarea id="description" name="description" rows="5"></textarea>
+		<textarea id="description" name="description" rows="5">{{ old('description') }}</textarea>
 		@error('description')
 			<span class="error-message">{{ $message }}</span>
 		@enderror
 	</div>
 	<div>
 		<label for="long_description">Long Description</label>
-		<textarea id="long_description" name="long_description" rows="10"></textarea>
+		<textarea id="long_description" name="long_description" rows="10">{{ old('long_description') }}</textarea>
 		@error('long_description')
 			<span class="error-message">{{ $message }}</span>
 		@enderror
