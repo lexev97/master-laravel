570945371e0d4def7e7769651e128049e64c72ad
Lexev <evtenkoaleksei@gmail.com>
2024-04-10 19:10:03 +0300

    62. Rating and Review Count on All Pages

--- book-review/app/Http/Controllers/BookController.php
+++ book-review/app/Http/Controllers/BookController.php
@@ -27,11 +27,17 @@ class BookController extends Controller
             'popular_last_6months' => $books->popularLast6Months(),
             'highest_rated_last_month' => $books->highestRatedLastMonth(),
             'highest_rated_last_6months' => $books->highestRatedLast6Months(),
-            default => $books->latest()
+            default => $books->latest()->withAvgRating()->withReviewsCount()
         };
 
         $cacheKey = 'books:' . $filter . ':' . $title;
-        $books = cache()->remember($cacheKey, 3600, fn () => $books->get());
+        $books =
+            // cache()->remember(
+            // $cacheKey,
+            // 3600,
+            // fn() =>
+            $books->get();
+        // );
 
         return view('books.index', ['books' => $books]);
     }
@@ -63,7 +69,7 @@ class BookController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Book $book)
+    public function show(int $id)
     {
         return view(
             'books.show',
@@ -73,11 +79,16 @@ class BookController extends Controller
                 ])
             ]
         );
-        $cacheKey = 'book:' . $book->id;
+        $cacheKey = 'book:' . $id;
 
-        $book = cache()->remember($cacheKey, 3600, fn () => $book->load([
-            'reviews' => fn ($query) => $query->latest()
-        ]));
+        $book = cache()->remember(
+            $cacheKey,
+            3600,
+            fn () =>
+            Book::with([
+                'reviews' => fn ($query) => $query->latest()
+            ])->withAvgRating()->withReviewsCount()->findOrFail($id)
+        );
 
         return view('books.show', ['book' => $book]);
     }
