0ef92e034867cd7eb16a22913c9656c92df4e7c2
Lexev <evtenkoaleksei@gmail.com>
2024-04-11 12:23:22 +0300

    64. Scoped Resource Controller - Adding a Review

--- book-review/routes/web.php
+++ book-review/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,7 @@ Route::get('/', function () {
 
 Route::resource('books', BookController::class)
     ->only(['index', 'show']);
+
+Route::resource('books.reviews', ReviewController::class)
+    ->scoped(['review' => 'book'])
+    ->only(['create', 'store']);
