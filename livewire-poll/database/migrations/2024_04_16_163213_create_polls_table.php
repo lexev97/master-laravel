8aba6ad5c5f6d8a379b65c53025718620c26f3c7
Lexev <evtenkoaleksei@gmail.com>
2024-04-16 19:42:33 +0300

    92. Your First Livewire Component

--- livewire-poll/database/migrations/2024_04_16_163213_create_polls_table.php
+++ livewire-poll/database/migrations/2024_04_16_163213_create_polls_table.php
@@ -1,22 +1,20 @@
+@@ -0,0 +1,27 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('options', function (Blueprint $table) {
+        Schema::create('polls', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('title');
             $table->timestamps();
-
-            $table->foreignIdFor(\App\Models\Poll::class)->constrained();   
         });
     }
 
@@ -25,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('options');
+        Schema::dropIfExists('polls');
     }
 };
--- livewire-poll/database/migrations/2024_04_16_163215_create_options_table.php
+++ livewire-poll/database/migrations/2024_04_16_163215_create_options_table.php
@@ -4,18 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('votes', function (Blueprint $table) {
-            $table->id();            
+        Schema::create('options', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
             $table->timestamps();
 
-            $table->foreignIdFor(\App\Models\Option::class)->constrained();
+            $table->foreignIdFor(\App\Models\Poll::class)->constrained();
         });
     }
 
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('votes');
+        Schema::dropIfExists('options');
     }
 };
