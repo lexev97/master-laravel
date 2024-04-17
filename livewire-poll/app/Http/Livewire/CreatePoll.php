f58ec030c37ec0daab51665c7dcac5f387e8fce5
Lexev <evtenkoaleksei@gmail.com>
2024-04-17 10:24:38 +0300

    94. Actions in Livewire

--- livewire-poll/app/Http/Livewire/CreatePoll.php
+++ livewire-poll/app/Http/Livewire/CreatePoll.php
@@ -7,9 +7,16 @@ use Livewire\Component;
 class CreatePoll extends Component
 {
     public $title;
+    public $options = ['First'];
 
     public function render()
     {
         return view('livewire.create-poll');
     }
+
+    public function addOption()
+    {
+        $this->options[] = '';
+    }
+
 }
