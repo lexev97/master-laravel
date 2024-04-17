6dc287c96c58cc9ec9a24916da6351ebc77b3868
Lexev <evtenkoaleksei@gmail.com>
2024-04-17 11:06:49 +0300

    98. Validation in Livewire

--- livewire-poll/resources/views/livewire/create-poll.blade.php
+++ livewire-poll/resources/views/livewire/create-poll.blade.php
@@ -4,23 +4,29 @@
 
         <input type="text" wire:model="title" />
 
+        @error('title')
+        <div class="text-red-500">{{ $message }}</div>
+        @enderror
+
         <div class="mb-4 mt-4">
             <button class="btn" wire:click.prevent="addOption">Add Option</button>
-          </div>
-      
-          <div>
+        </div>
+
+        <div>
             @foreach ($options as $index => $option)
-              <div class="mb-4">
+            <div class="mb-4">
                 <label>Option {{ $index + 1 }}</label>
                 <div class="flex gap-2">
-                  <input type="text" wire:model="options.{{ $index }}" />
-                  <button class="btn"
-                    wire:click.prevent="removeOption({{ $index }})">Remove</button>
+                    <input type="text" wire:model="options.{{ $index }}" />
+                    <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
                 </div>
-              </div>
+                @error("options.{$index}")
+                <div class="text-red-500">{{ $message }}</div>
+                @enderror
+            </div>
             @endforeach
-          </div>
+        </div>
 
-          <button type="submit" class="btn">Create Poll</button>
+        <button type="submit" class="btn">Create Poll</button>
     </form>
 </div>
