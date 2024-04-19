e40c7944d1913a4e94e444eaf8b2de2eb61015ef
Lexev <evtenkoaleksei@gmail.com>
2024-04-19 14:47:46 +0300

    108. Jobs Page: Tag Component & Job Info

--- book-review/app/Providers/AuthServiceProvider.php
+++ book-review/app/Providers/AuthServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use App\Models\Attendee;
+use App\Models\Event;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -23,8 +25,13 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies();
-
-        //
+        Gate::define("update-event", function ($user, Event $event) {
+            return $user->id === $event->user_id;
+        });
+        
+        Gate::define("delete-attendee", function ($user, Event $event, Attendee $attendee) {
+            return $user->id === $event->user_id ||
+                $user->id === $attendee->user_id;
+        });
     }
 }
