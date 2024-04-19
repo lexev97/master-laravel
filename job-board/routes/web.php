13be6323a83404f4c886bb7eb982115ea71e3c38
Lexev <evtenkoaleksei@gmail.com>
2024-04-19 15:07:19 +0300

    109. Job Page: Job Card & Link Button Components

--- job-board/routes/web.php
+++ job-board/routes/web.php
@@ -13,5 +13,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('', fn() => to_route('jobs.index'));
 
-Route::resource('jobs', JobController::class)->only(['index']);
+Route::resource('jobs', JobController::class)->only(['index', 'show']);
