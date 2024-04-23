60b149970069dbde9713eb23df13454a3ce91a7d
Lexev <evtenkoaleksei@gmail.com>
2024-04-23 15:55:28 +0300

    132. Applying for Jobs: The Job Policy

--- job-board/resources/views/job/show.blade.php
+++ job-board/resources/views/job/show.blade.php
@@ -4,31 +4,34 @@
 		<p class="mb-4 text-sm text-slate-500">
 			{!! nl2br(e($job->description)) !!}
 		</p>
+		<x-link-button :href="route('job.application.create', $job)">
+			Apply
+		</x-link-button>
 	</x-job-card>
 
 	<x-card class="mb-4">
 		<h2 class="mb-4 text-lg font-medium">
-		  More {{ $job->employer->company_name }} Jobs
+			More {{ $job->employer->company_name }} Jobs
 		</h2>
-	
+
 		<div class="text-sm text-slate-500">
-		  @foreach ($job->employer->jobs as $otherJob)
+			@foreach ($job->employer->jobs as $otherJob)
 			<div class="mb-4 flex justify-between">
-			  <div>
-				<div class="text-slate-700">
-				  <a href="{{ route('jobs.show', $otherJob) }}">
-					{{ $otherJob->title }}
-				  </a>
+				<div>
+					<div class="text-slate-700">
+						<a href="{{ route('jobs.show', $otherJob) }}">
+							{{ $otherJob->title }}
+						</a>
+					</div>
+					<div class="text-xs">
+						{{ $otherJob->created_at->diffForHumans() }}
+					</div>
 				</div>
 				<div class="text-xs">
-				  {{ $otherJob->created_at->diffForHumans() }}
+					${{ number_format($otherJob->salary) }}
 				</div>
-			  </div>
-			  <div class="text-xs">
-				${{ number_format($otherJob->salary) }}
-			  </div>
 			</div>
-		  @endforeach
+			@endforeach
 		</div>
-	  </x-card>
+	</x-card>
 </x-layout>
