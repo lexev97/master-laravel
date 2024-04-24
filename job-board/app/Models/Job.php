de59e5bca7ad5504aa38ff6dfd41e7abd594b160
Lexev <evtenkoaleksei@gmail.com>
2024-04-24 12:20:17 +0300

    149. Employer: Soft Deletes

--- job-board/app/Models/Job.php
+++ job-board/app/Models/Job.php
@@ -8,11 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Job extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'title',
