45f277bbb2c360f998b161b83e6e2e78ab31d257
Lexev <evtenkoaleksei@gmail.com>
2024-04-24 11:36:28 +0300

    145. Employer: Adding Jobs Logic

--- job-board/resources/views/components/radio-group.blade.php
+++ job-board/resources/views/components/radio-group.blade.php
@@ -8,8 +8,14 @@
 
   @foreach ($optionsWithLabels as $label => $option)
   <label for="{{ $name }}" class="mb-1 flex items-center">
-    <input type="radio" name="{{ $name }}" value="{{ $option }}" @checked($option === ($value ?? request($name))) />
+    <input type="radio" name="{{ $name }}" value="{{ $option }}" @checked($option===($value ?? request($name))) />
     <span class="ml-2">{{ $label }}</span>
   </label>
   @endforeach
+  
+  @error($name)
+  <div class="mt-1 text-xs text-red-500">
+    {{ $message }}
+  </div>
+  @enderror
 </div>
